d15f3adee04b7c0385ff0214e6f1ed3225c3db83
alexandra <agonzalez1055@alumnes.politecnicllevant.cat>
2025-09-26 11:46:37 +0200

    Vistas las variables, y concatenacion

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+
+    <?php
+    /* Las Variables pueden hacerse con $nombreVariable*/
+        $saludo = "Hola";
+    /* Si usamos comilla simple, ' ', el texto aparece tal cual, con las comillas dobles interpreta lo que le pasamos*/
+        echo $saludo . " mundo";
+    /* Es importante la refactorizacion del codigo para que ssea limpio*/
+    ?>
+</h1>
+</body>
+</html>
