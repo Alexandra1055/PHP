74fa56af3d40d2562e8a923a3798f39d9992e2aa
alexandra <agonzalez1055@alumnes.politecnicllevant.cat>
2025-10-21 19:50:59 +0200

    creamos una base de datos, y empezamos a ver las clases

--- index.php
+++ index.php
@@ -3,3 +3,22 @@
 require 'functions.php';
 
 require 'router.php';
+
+//conectamos nuestra MySQL database
+
+class Person {
+    public $name;
+    public $age;
+
+    public function respirar() //la visibilidad predeterminada es public pero mejor ponerlo siempre
+    {
+        echo $this->name . ' esta respirando';
+    }
+}
+
+$person = new Person();
+
+$person->name = "Alexandra"; //Al hacer referencia a una propiedad no uso el $
+$person->age = 31;
+
+dd($person->respirar());
