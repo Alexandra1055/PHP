147eea36abf7f5c0dceee727c487be5c17ce2310
alexandra <agonzalez1055@alumnes.politecnicllevant.cat>
2025-10-21 20:34:52 +0200

    funcion de query creada

--- index.php
+++ index.php
@@ -2,18 +2,31 @@
 
 require 'functions.php';
 
-require 'router.php';
+//require 'router.php';
 
-//conectamos nuestra MySQL database
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
 
-$pdo= new PDO($dsn);
 
-$declaracion= $pdo->prepare("SELECT * FROM posts");
+//conectamos nuestra MySQL database y ejecutamos una consulta y la construimos(RECORDAR __)
 
-$declaracion->execute();
+class DataBase{
+    public function query($query){
+        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
 
-$posts = $declaracion->fetchAll(PDO::FETCH_ASSOC);
+        $pdo= new PDO($dsn);
+
+        $declaracion= $pdo->prepare($query);
+
+        $declaracion->execute();
+
+        return $declaracion->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+$db = new DataBase();
+$posts =$db->query("SELECT * FROM posts where id=1");
+
+
+
 
 foreach ($posts as $post) {
     echo "<li>" . $post['title'] . "</li>";
