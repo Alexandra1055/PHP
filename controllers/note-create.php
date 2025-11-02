c8b3efa5bc39b8bd1bd59107bffcf0f4c1503730
alexandra <agonzalez1055@alumnes.politecnicllevant.cat>
2025-11-02 16:41:00 +0100

    control de errores, texto vacio

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,18 @@ $config = require ('config.php');
 $db = new DataBase($config['database']);
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $db ->query('INSERT INTO notes(body,user_id) VALUES(:body,:user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1]
-    );
+    $errors = [];
+
+    if(strlen($_POST['body']) == 0){
+        $errors['body'] = 'El texto es obligatorio';
+    }
+
+    if(empty($errors)){
+        $db ->query('INSERT INTO notes(body,user_id) VALUES(:body,:user_id)', [
+                'body' => $_POST['body'],
+                'user_id' => 1]
+        );
+    }
 }
 
 require 'views/note-create.view.php';
