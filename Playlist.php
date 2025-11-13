9ffada386aa2ea3834cf02a820d1638412f53146
Alexandra <agonzalez1055@alumnes.politecnicllevant.cat>
2025-11-13 11:23:09 +0100

    pasamos la propiedad de los atributos para saber de que son

--- Playlist.php
+++ Playlist.php
@@ -6,20 +6,34 @@ class Playlist
     public $name;
     public $songs;
 
-    public function __construct($name, $songs)
+    public function __construct(public string $name, public array $songs)
     {
-        $this->name = $name;
-        $this->songs = $songs;
+        //$this->name = $name;
+        //$this->songs = $songs;
     }
 
 }
 
+class Song{
+    public function __construct(public string $name, public string $artist ){
 
+    }
+}
+
+
+/*
 $playlist = new Playlist("80s Headbangers", [
     'Black in Black',
     'Are you ready',
     'Hells Bells',
     'Highway to Hell'
 ]);
+*/
 
-die(var_dump($playlist));
+$songs = [
+    new Song('My Heart Will Go On','Celine Dion')
+];
+
+$playlist = new Playlist('90s Movie Soundtracks',$songs);
+
+var_dump($playlist->songs[0]->artist);
