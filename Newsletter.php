cc0de15004bf8fb32ec3e6d64e6ac2adb0193ac3
Alexandra <agonzalez1055@alumnes.politecnicllevant.cat>
2025-11-14 11:50:41 +0100

    creamos una interfaz con dos clases implementandola

--- Newsletter.php
+++ Newsletter.php
@@ -3,17 +3,35 @@ class User{
 
 }
 class Newsletter{
-    public function subscribe(User $user){
-        //interactua con el monitor
-        $cm = new CampaingMonitor();
-        $cm->addApiKey('abc');
-        $list = $cm -> lists -> find('default');
-        $list->addToList($user->email);
+    public function subscribe(User $user, NewsletterProvider $provider){
 
+        $provider->addToList('default', $user->email);
         //Actualiza el usuario y lo marca como suscrito
         $user->update(['suscribed' => true]);
 
         return true;
+    }
+}
+
+interface NewsletterProvider{
+    public function addToList(string $list, string $email):void;
+}
+
+class CampaingMonitor implements NewsletterProvider {
+    public function addToList(string $list, string $email):void{
+        //interactua con el monitor de camapaña
+        $cm = new CampaingMonitorAPI();
+        $cm->addApiKey('abc');
+        $list = $cm -> lists -> find($list);
+        $list->addToList($email);
+    }
+}
+
+class PostmarkProvider implements NewsletterProvider {
+    public function addToList(string $list, string $email):void{
+        //interactua con el monitor de camapaña
+        $cm = new PostmarkAPI('defg');
+        $list = $cm->addToDefaultList($email);
     }
 }
 
